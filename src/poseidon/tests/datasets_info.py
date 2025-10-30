c3ccdce4041c44c727715503b2956f8451d7c3b2
Q. T. Felix <53819958+Quant-TheodoreFelix@users.noreply.github.com>
2025-10-30 16:42:12 +0900

    fixed Dataset Information test

--- src/poseidon/tests/datasets_info.py
+++ src/poseidon/tests/datasets_info.py
@@ -1,4 +1,14 @@
-from poseidon.data.dataset import read_dataset
+import os
+import sys
+# 로컬 모듈 호출 (이 파일 기준: ../data 하위 모듈 호출)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# 상위 디렉터리 경로를 sys.path에 추가 (../)
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+import data
+from data.dataset import read_dataset
+from tqdm import tqdm
 
 
 datasets = [
@@ -7,10 +17,13 @@ datasets = [
     "NF-ToN-IoT-v3",
     "NF-UNSW-NB15-v3",
 ]
-for dataset in datasets:
-    print(f"================================================")
-    print(f"{dataset} 데이터셋 정보")
-    dask_df = read_dataset(dataset)
-    print(dask_df.info())
-    print(dask_df["Label"].value_counts())
-    print(f"================================================")
+if __name__ == "__main__":
+    for dataset in datasets:
+        print("=" * 45)
+        print(f"{dataset} 데이터셋 정보")
+        dask_df = read_dataset(dataset)
+
+        # Label 이외 모든 행 제거
+        dask_df = dask_df.drop(columns=[col for col in dask_df.columns if col != "Label"])
+        print(dask_df["Label"].value_counts().compute())
+        print("=" * 45)
